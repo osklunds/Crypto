619052da1121babbd6f981323e18c387485b16b7
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2018-11-24 18:24:55 +0100

    Add gcd and eea

---
+++ Source/Math.hs
@@ -0,0 +1,40 @@
+
+module Math
+( divides
+, gcd
+, eea
+)
+where
+
+import Prelude hiding (gcd)
+import Test.QuickCheck
+
+divides :: (Integral a) => a -> a -> Bool
+a `divides` b = b `mod` a == 0
+
+gcd :: (Integral a) => a -> a -> a
+gcd a b
+  | a < 0 || b < 0 = gcd (abs a) (abs b)
+  | b > a          = gcd b a
+  | b == 0         = a
+  | b `divides` a  = b
+  | otherwise      = gcd b (a `mod` b)
+
+-- Given a and b, returns (d,s,t) s.t. gcd(a,b)=d=as+bt
+-- a and b must be positive
+eea :: (Integral a) => a -> a -> (a, a, a)
+eea a b
+  | a == 0 || b == 0 = error "zero as argument"
+  | a == b = (a,1,0)
+  | r == 0 = (b,0,1)
+  | r /= 0 = (d,s,t)
+  where
+    r = a `mod` b
+    q = a `div` b
+    (d,s',t') = eea b r
+    (s,t) = (t',s'-q*t')
+
+test_eea :: Int -> Int -> Property
+test_eea a b = a > 0 && b > 0 ==> gcd a b == d
+  where
+    (d,s,t) = eea a b
