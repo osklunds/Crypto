c85e77c0e30385d39526805750293ed3e14d4ba5
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2018-11-24 20:03:10 +0100

    Add coprime and inverse modulo

--- Source/Math/GCD.hs
+++ Source/Math/GCD.hs
@@ -3,16 +3,18 @@ module Math.GCD
 ( divides
 , gcd
 , eea
+, coprime
+, invMod
 )
 where
 
 import Prelude hiding (gcd)
 import Test.QuickCheck
 
-divides :: (Integral a) => a -> a -> Bool
+divides :: Integral a => a -> a -> Bool
 a `divides` b = b `mod` a == 0
 
-gcd :: (Integral a) => a -> a -> a
+gcd :: Integral a => a -> a -> a
 gcd a b
   | a < 0 || b < 0 = gcd (abs a) (abs b)
   | b > a          = gcd b a
@@ -22,7 +24,7 @@ gcd a b
 
 -- Given a and b, returns (d,s,t) s.t. gcd(a,b)=d=as+bt
 -- a and b must be positive
-eea :: (Integral a) => a -> a -> (a, a, a)
+eea :: Integral a => a -> a -> (a, a, a)
 eea a b
   | a == 0 || b == 0 = error "zero as argument"
   | a == b = (a,1,0)
@@ -35,6 +37,23 @@ eea a b
     (s,t) = (t',s'-q*t')
 
 test_eea :: Int -> Int -> Property
-test_eea a b = a > 0 && b > 0 ==> gcd a b == d
+test_eea a b = a > 0 && b > 0 ==> gcd a b == d && a*s+b*t == d
   where
     (d,s,t) = eea a b
+
+-- Multiplicative inverse of a mod m
+invMod :: Integral a => a -> a -> a
+a `invMod` m
+  | d /= 1    = error "Not coprime"
+  | otherwise = s `mod` m
+  where
+    (d,s,_) = eea a m
+
+coprime :: Integral a => a -> a -> Bool
+a `coprime` b = gcd a b == 1
+
+test_invMod :: Int -> Int -> Property
+test_invMod a m = a > 0 && m > 2 && a `coprime` m ==>
+  i > 0 && i < m && a*i `mod` m == 1
+  where
+    i = a `invMod` m
