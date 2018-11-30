abddada4e3fe63b372192b4f58cb7658bda27d61
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2018-11-30 21:01:05 +0100

    PowerModulo use new BigInt for test

--- Source/Math/PowerModulo.hs
+++ Source/Math/PowerModulo.hs
@@ -4,12 +4,15 @@ module Math.PowerModulo
 )
 where
 
+import Math.BigInt
+
 import Test.QuickCheck
 
 -- powerModulo b e m calculates b^e mod m efficiently
 powerModulo :: (Integral a) => a -> a -> a -> a
 powerModulo b e m
-  | b < 0 || e < 0 || m < 2 = error "Bad arguments"
+  | e < 0 || m < 2 = error "Bad arguments"
+  | b < 0                   = powerModulo (b `mod` m) e m
   | e == 0                  = 1
   | b == 0 || b == 1        = b
   | b == m                  = 0
@@ -33,22 +36,8 @@ maximumPower b e m
     list = [(b^x,x) | x <- [1..e], b^x >= m]
     (a,x) = head $ list
 
-newtype BigInt = BigInt Integer 
-               deriving Show
-
-instance Arbitrary BigInt where
-  arbitrary = do
-    i <- choose (2,100000)
-    return $ BigInt i
-
-unwrap :: BigInt -> Integer
-unwrap (BigInt i) = i
-
-test_powerModulo :: BigInt -> BigInt -> BigInt -> Bool
-test_powerModulo b' e' m' = r == (b^e `mod` m) && r >= 0 && r < m
-
+prop_powerModulo :: BigInt100000 -> BigInt100000 -> BigInt100000 -> Property
+prop_powerModulo b e m = e >= 0 && m >= 2 ==> correct
   where
-    b = unwrap b'
-    e = unwrap e'
-    m = unwrap m'
     r = powerModulo b e m
+    correct = r == (b^e `mod` m) && r >= 0 && r < m
