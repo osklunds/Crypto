bba6719a35961c67c0a98affad5b290a5c2297c6
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2018-12-20 10:04:30 +0100

    Add message schedule array

--- src/Hash/SHA256.hs
+++ src/Hash/SHA256.hs
@@ -100,4 +100,30 @@ comp :: CompVars ->
         (Word32 -> Word32) ->
         CompVars
 comp = comp' 0
+
+
+
+ws :: [Word32] -> [Word32]
+ws cnk = list
+  where
+    list = cnk ++ map w [16..63]
+
+    w i = w16 + s0 + w7 + s1
+      where
+        w16 = list !! (i-16)
+        w15 = list !! (i-15)
+        w7  = list !! (i-7)
+        w2  = list !! (i-2)
+
+        s0 = (w15 `rotateR` 7 ) `xor`
+             (w15 `rotateR` 18) `xor`
+             (w15 `shiftR`  3 )
+        s1 = (w2  `rotateR` 17) `xor`
+             (w2  `rotateR` 19) `xor`
+             (w2  `shiftR`  10)
+
+w :: [Word32] -> Word32 -> Word32
+w cnk i = ws cnk !! (fromIntegral i)
+
+
       
