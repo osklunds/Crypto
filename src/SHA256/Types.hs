5a7114b31924ea1eeca62a4edc8d8505259ae0ec
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2018-12-24 10:29:50 +0100

    Clearer types for compression

--- src/SHA256/Types.hs
+++ src/SHA256/Types.hs
@@ -1,9 +1,11 @@
 
 module SHA256.Types
 ( BitString
+, Comp
 )
 where
 
 import Data.Word
 
-type BitString = [Word32]
+type BitString = [Word32] -- Arbitrary length
+type Comp      = [Word32] -- 8 32-bit words
