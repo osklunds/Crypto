cbcc83ca5140e55f38814a59177d11d24e3c05f2
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2018-12-25 08:01:00 +0100

    Minor changes padding

--- src/SHA256/Pad.hs
+++ src/SHA256/Pad.hs
@@ -22,14 +22,9 @@ msb b = last [fromIntegral i | i <- [0..7], testBit b i]
 addOne :: BitString -> BitString
 addOne = (++ [128])
 
--- Returns the length in bits of the bitstring.
--- There must be no zero-bytes leading.
-bitLength :: BitString -> Word64
-bitLength []     = 0
-bitLength (b:bs) = msb b + 1 + 8 * length bs
 
 -- Returns the length in bits, of the length the bitstring
--- should have after 0:s have been prepended
+-- should have after 0:s have been appended
 targetBitLength :: Integral a => a -> a
 targetBitLength len
   -- We can fit another 64 bits before exceeding block length 512
@@ -47,15 +42,24 @@ addZeros len bs = take tarLen $ bs ++ repeat 0
     -- Length in blocks
     tarLen = targetBitLength len `div` 8
 
+
+-- Returns the length in bits of the bitstring.
+-- There must be no zero-bytes leading.
+bitLength :: BitString -> Word64
+bitLength []     = 0
+bitLength (b:bs) = msb b + 1 + 8 * length bs
+
 -- Represents a 64-bit word as a list of 8-bit words
 w64ToW8List :: Word64 -> BitString
 w64ToW8List w64 = take 8 [fromIntegral (w64 `shiftR` i) 
                          | i <- [56,48..]]
 
+
 pad :: BitString -> BitString
-pad bs = ((addZeros origLen). addOne) bs ++ w64ToW8List toUse
+pad bs = addZeros after1Len (addOne bs) ++ w64ToW8List toUse
   where
     origLen = bitLength bs
+    after1Len = bitLength (addOne bs)
     byteLen = origLen `div` 8
     toUse   = if origLen == byteLen * 8
               then origLen
--- src/SHA256/Types.hs
+++ src/SHA256/Types.hs
@@ -10,7 +10,7 @@ where
 
 import Data.Word
 
-type BitString = [Word8] -- Arbitrary length
+type BitString = [Word8]  -- Arbitrary length
 type Comp      = [Word32] -- 8 32-bit words
 type Chunk     = [Word32] -- 16 32-bit words, the 512-bit chunk
 type RoundWords= [Word32] -- 64 32-bit words, one per round
