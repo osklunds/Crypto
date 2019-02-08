722b96b95bd8d7692ec2d8900de560fd1ddaf6d8
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-02-08 20:00:43 +0100

    Data types for parameters and shares in Shamir

--- src/Shamir.hs
+++ src/Shamir.hs
@@ -36,21 +36,50 @@ prop_poly cs q x = q >= 2 ==> sum1 == sum2
     sum2 = (sum $ zipWith (\c e -> c*x^e) cs [0..]) `mod` q
 
 
--- | share s n t q computes the n secret shares of s, where
--- the threshold is t, modulo q.
+-- | Couples a share si to the ID pi.
+data ShareIdPair a = ShareIdPair a a
+                   deriving (Eq)
+
+instance Show a => Show (ShareIdPair a) where
+  show (ShareIdPair s p) = "<share=" ++ 
+                            show s ++ 
+                            ",id=" ++ 
+                            show p ++ 
+                            ">"
+
+-- | Parameters n (number of parties), t (threshold) and q (modulo).
+data ShamirParams a = ShamirParams a a a
+                    deriving (Eq)
+
+instance Show a => Show (ShamirParams a) where
+  show (ShamirParams n t q) = "ShamirParams n=" ++ 
+                              show n ++ 
+                              " t=" ++ 
+                              show t ++ 
+                              " q=" ++ 
+                              show q
+
+-- In the beta function, inverses of numbers
+-- at most n will be calculated mod q. So if q is prime > n
+-- such inverses will exist.
+validParams :: (Integral a, Random a) => 
+  ShamirParams a -> Bool
+validParams (ShamirParams n t q) = t >= 1
+                                && n > t
+                                && q >= 2
+                                && prime q
+                                && n < q
+
 share :: (Integral a, Random a, MonadRandom m) => 
-  a -> a -> a -> a -> m [a]
-share s n t q
-  | not (t >= 1 && n > t && q >= 2) = error "Invalid arguments"
-  | not (prime q) || n >= q         = error "Bad modulus"
-  -- The reason is, in beta function, inverses of numbers
-  -- at most n will be calculated mod q. So if q is prime > n
-  -- such inverses will exist.
+  a -> ShamirParams a -> m [ShareIdPair a]
+share s p@(ShamirParams n t q)
+  | s /= s `mod` q      = error "s not in Zq"
+  | not (validParams p) = error "Invalid parameters"
   | otherwise = do
       infCoeffs   <- getRandomRs (0,q-1)
       let coeffs  =  s:(take t infCoeffs)
           polyFun =  poly coeffs q
-      return $ map polyFun [1..n]
+      return [ShareIdPair (polyFun pi) pi | pi <- [1..n]]
 
 
 -- beta js q i calculates beta i, when parties js are 
@@ -89,36 +118,39 @@ prop_beta g n ne q = n' >= 1 ==> s == s'
     s' = (sum $ zipWith (*) bVals yVals) `mod` q'
 
 
--- | recover si_s pi_s q, recovers the share using the
--- shares si_s from parties pi_s, modulo q.
-recover :: Integral a => [a] -> [a] -> a -> a
-recover si_s pi_s q = sum prods `mod` q
+recover :: Integral a => [ShareIdPair a] -> ShamirParams a -> a
+recover pairs (ShamirParams n t q) = sum prods `mod` q
   where
+    si_s  = map (\(ShareIdPair si _) -> si) pairs
+    pi_s  = map (\(ShareIdPair _ pi) -> pi) pairs
     bi_s  = map (beta pi_s q) pi_s
     prods = zipWith (*) bi_s si_s
 
+
+instance (Integral a, Random a, Arbitrary a) => 
+  Arbitrary (ShamirParams a) where
+    arbitrary = do
+      (n,t,q) <- arbitrary
+      let n' = n `mod` 40 + 2           -- Performance
+          t' = (t `mod` (n'-1)) + 1     -- Performance
+          q' = nextPrime (max q n' + 1) -- Correctness
+      return $ ShamirParams n' t' q'
+
 prop_shareRecover :: StdGen -> 
                      BigInt100000 -> 
-                     BigInt100000 -> 
-                     BigInt100000 -> 
-                     BigInt100000 -> 
-                     Property
-prop_shareRecover g s n t q = n' > 0 && t' >= 1 && n' > t' ==>
-  s_rec == s'
+                     ShamirParams BigInt100000 ->
+                     Bool
+prop_shareRecover g s p@(ShamirParams n t q) = s_rec == s'
   where
-    n' = n `mod` 40 + 2         -- Performance
-    t' = (t `mod` (n'-1)) + 1   -- Performance
-    q' = nextPrime (max q n'+1) -- Correct arguments
-    s' = s `mod` q'             -- Performance
+    s' = s `mod` q -- Correctness
     
-    (si_sAll, pi_s) = fst $ runRand f g
+    (allPairs, pi_s) = fst $ runRand f g
       where
         f = do
-          si_sAll <- share s' n' t' q'
-          -- Picks between t'+1 and n' parties that collaborate
-          numP    <- getRandomR   (t'+1,n')
-          pi_s    <- getRandomRsU (1,n') numP
-          return (si_sAll, pi_s)
+          allPairs   <- share s' p
+          numParties <- getRandomR   (t+1,n)
+          pi_s       <- getRandomRsU (1,n) numParties
+          return (allPairs, pi_s)
 
-    si_s  = [si_sAll !! (pi-1) | pi <- pi_s]
-    s_rec = recover si_s pi_s q'
+    pairs = [allPairs !! (pi-1) | pi <- pi_s]
+    s_rec = recover pairs p
