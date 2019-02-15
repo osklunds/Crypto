7e95e586566be81f72d68c98de2293dc8a7e539c
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-02-15 19:52:50 +0100

    Simplify outermost sha256

--- src/SHA256.hs
+++ src/SHA256.hs
@@ -7,8 +7,7 @@ where
 import Data.Word
 import Data.Bits
 
-import SHA256.Pad
-import SHA256.MerkleDamgard
+import SHA256.Padding
 import SHA256.Constants
 import SHA256.Compression
 import SHA256.MessageSchedule
@@ -17,28 +16,16 @@ import SHA256.RepConv
 
 import Tools
 
--- IV to use for Merkle-Damgård
-mdIV :: Digest
-mdIV = map iv [0..7]
-
--- Compression function to use for Merkle-Damgård
-mdComp :: Digest -> Chunk -> Digest
-mdComp digest ms = comp digest k (wc ms)
-
--- Inner hashing using Merkle-Damgård
-inner :: [Chunk] -> Digest
-inner = merkleDamgard mdComp mdIV
-
-
--- Outer hashing using padding, and then Merkle-Damgård
-outer :: BitString -> Digest
-outer ms = hashed
+processChunks :: Digest -> [Chunk] -> Digest
+processChunks d []     = d
+processChunks d (c:cs) = processChunks (compress ks ws d) cs
   where
-    padded = map w8sToW32 $ group 4 $ pad ms
-    -- Split padded messages into 512-bit chunks of 32-bit words
-    chunks = group 16 padded
-    hashed = inner chunks
+    ks = roundConstants
+    ws = messageSchedule c
+
+padAndChunk :: BitString -> [Chunk]
+padAndChunk = group 16 . map w8sToW32 . group 4 . pad
 
 -- The hash of an ascii string, as a string of hex numbers
 sha256 :: String -> String
-sha256 = w32sToHex . outer . asciiToW8s
+sha256 = w32sToHex . processChunks initialHash . padAndChunk . asciiToW8s
