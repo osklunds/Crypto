409317d8771ee3c0fcac03c298b8e4aa4f0190e1
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-02-15 19:30:14 +0100

    Simplify constans and message schedule

--- src/SHA256/MessageSchedule.hs
+++ src/SHA256/MessageSchedule.hs
@@ -1,36 +1,27 @@
 
 module SHA256.MessageSchedule
-( wc
+( messageSchedule
 )
 where
 
 import Data.Word
 import Data.Bits
 
-import SHA256.Types
+s0 :: Word32 -> Word32
+s0 w15 = (w15 `rotateR` 7 ) `xor`
+         (w15 `rotateR` 18) `xor`
+         (w15 `shiftR`  3 )
+s1 :: Word32 -> Word32
+s1 w2  = (w2  `rotateR` 17) `xor`
+         (w2  `rotateR` 19) `xor`
+         (w2  `shiftR`  10)
 
--- List of the message schedule array.
--- A list of 64 32-bit words
-ws :: Chunk -> RoundWords
-ws cnk = list
-  where
-    list = cnk ++ map w [16..63]
-
-    w i = w16 + s0 + w7 + s1
-      where
-        w16 = list !! (i-16)
-        w15 = list !! (i-15)
-        w7  = list !! (i-7)
-        w2  = list !! (i-2)
-
-        s0 = (w15 `rotateR` 7 ) `xor`
-             (w15 `rotateR` 18) `xor`
-             (w15 `shiftR`  3 )
-        s1 = (w2  `rotateR` 17) `xor`
-             (w2  `rotateR` 19) `xor`
-             (w2  `shiftR`  10)
-
--- Message schedule array as a function.
--- Takes an index in [0..63]
-wc :: Integral a => Chunk -> a -> Word32
-wc cnk i = ws cnk !! (fromIntegral i)
+messageSchedule :: [Word32] -> [Word32]
+messageSchedule chunk = let list = chunk ++ map ms [16..63]
+                            ms i = w16 + s0 w15 + w7 + s1 w2
+                              where
+                                w16 = list !! (i-16)
+                                w15 = list !! (i-15)
+                                w7  = list !! (i-7)
+                                w2  = list !! (i-2)
+                        in list
