84e2d8fae28adfd40ee28a5df14bb907b9df99fd
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-02-15 19:29:43 +0100

    Simplify padding

--- src/SHA256/Padding.hs
+++ src/SHA256/Padding.hs
@@ -1,6 +1,6 @@
 
 module SHA256.Padding
-( -- pad
+( pad
 )
 where
 
@@ -14,37 +14,23 @@ import Tools
 import SHA256.Types
 
 
-append1bit :: BitString -> BitString
-append1bit = (++ [128])
+appendOne :: BitString -> BitString
+appendOne = (++ [128])
 
 appendZeros :: BitString -> BitString
-appendZeros bs
-  | 512 `divides` (length bs * 8 + 64) = bs
-  | otherwise                          = appendZeros (bs ++ [0])
+appendZeros bitString
+  | isMultiple = bitString
+  | otherwise  = appendZeros (bitString ++ [0])
+  where
+    isMultiple = 512 `divides` (length bitString * 8 + 64)
 
--- Starts from 0. Only works for non-zero bytes.
-msbIndex ::Integral a => Word8 -> a
-msbIndex b = last [fromIntegral i | i <- [0..7], testBit b i]
 
--- There must be no zero-bytes leading.
-lengthInBits :: BitString -> Word64
-lengthInBits []     = 0
-lengthInBits (b:bs) = msbIndex b + 1 + 8 * length bs
-
--- Represents a 64-bit word as a list of 8-bit words
 w64ToW8List :: Word64 -> BitString
-w64ToW8List w64 = take 8 [fromIntegral (w64 `shiftR` i) 
-                         | i <- [56,48..]]
+w64ToW8List w64 = [fromIntegral (w64 `shiftR` i) | i <- [56,48..0]]
+
+appendLength :: BitString -> BitString -> BitString
+appendLength bs = (++ w64ToW8List (8 * length bs))
 
 
 pad :: BitString -> BitString
-pad bs = appendZeros  (append1bit bs) ++ w64ToW8List toUse
-  where
-    origLen = lengthInBits bs
-    
-    byteLen = origLen `div` 8
-    toUse   = if origLen == byteLen * 8
-              then origLen
-              else (byteLen+1) * 8
-
-
+pad bs = appendLength bs . appendZeros . appendOne $ bs
