ec0420e2a0bb8869dc98ad8d45e142761d981851
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-02-16 10:01:18 +0100

    Minor changes SHA256 test

--- src/SHA256/Test.hs
+++ src/SHA256/Test.hs
@@ -9,19 +9,17 @@ where
 import "cryptohash" Crypto.Hash.SHA256
 import Data.ByteString (pack, unpack, empty)
 import Test.QuickCheck
+import Tools
 
 import SHA256
 import SHA256.RepConv
-import Tools
 
-ref :: String -> String
-ref = w32sToHex . map w8sToW32 . group 4 . map fromIntegral . unpack . hash . pack . asciiToW8s
+sha256ref :: String -> String
+sha256ref = w32x8ToHex . map w8x4ToW32 . group 4 . 
+            map fromIntegral . unpack . hash . 
+            pack . asciiToW8list
 
 prop_equal :: [String] -> Bool
-prop_equal strs = sha256 str == ref str
+prop_equal strs = sha256 str == sha256ref str
   where
     str = concat strs
-
-
-
-
