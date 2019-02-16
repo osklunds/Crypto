74fd0b2eb11fb21fe6ae43eb587f0f8156a28e87
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-02-16 09:35:26 +0100

    Minor change SHA256 outer

--- src/SHA256.hs
+++ src/SHA256.hs
@@ -6,15 +6,14 @@ where
 
 import Data.Word
 import Data.Bits
+import Tools
 
+import SHA256.Types
 import SHA256.Padding
 import SHA256.Constants
 import SHA256.Compression
 import SHA256.MessageSchedule
-import SHA256.Types
 import SHA256.RepConv
-
-import Tools
 
 processChunks :: Digest -> [Chunk] -> Digest
 processChunks d []     = d
@@ -23,9 +22,8 @@ processChunks d (c:cs) = processChunks (compress ks ws d) cs
     ks = roundConstants
     ws = messageSchedule c
 
-padAndChunk :: BitString -> [Chunk]
+padAndChunk :: ByteString -> [Chunk]
 padAndChunk = group 16 . map w8sToW32 . group 4 . pad
 
--- The hash of an ascii string, as a string of hex numbers
 sha256 :: String -> String
 sha256 = w32sToHex . processChunks initialHash . padAndChunk . asciiToW8s
