8967b22359b946116c5e405de944f1e3c625c77a
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-09-14 08:59:17 +0200

    Polish power modulo.

--- src/Math/PowerModulo.hs
+++ src/Math/PowerModulo.hs
@@ -10,11 +10,22 @@ import Prelude hiding (length)
 import Math.BigInt
 import Tools
 
+-- powerModulo b e m calculates b^e modulo m efficiently.
+-- Requirements: e>=0 and m>=2.
+powerModulo :: Integral a => a -> a -> a -> a
+powerModulo b e m = foldr f 1 (toBin e)
+  where
+    f 0 z = z^2 `mod` m
+    f 1 z = f 0 z * b `mod` m
+
 -- Positive base-10 integer to LSB-first bit string.
 toBin :: Integral a => a -> [a]
 toBin 0 = []
 toBin n = let (q, r) = n `quotRem` 2
           in  r:(toBin q)
+
+prop_toBin :: BigInt7 -> Property
+prop_toBin n = n >= 0 ==> (==n) . toDec . reverse . toBin $ n
 
 -- MSB-first bit string to positive base-10 integer.
 toDec :: Integral a => [a] -> a
@@ -24,21 +35,9 @@ toDec' :: Integral a => [a] -> a
 toDec' [] = 0
 toDec' (b:bs) = b + 2 * toDec' bs
 
-prop_toBin :: BigInt10000000 -> Property
-prop_toBin n = n >= 0 ==> (==n) . toDec . reverse . toBin $ n
-
--- | powerModulo b e m calculates b^e modulo m efficiently.
-powerModulo :: Integral a => a -> a-> a -> a
-powerModulo b e m = foldr f 1 bin
-  where
-    bin = toBin e
-    len = length bin
-
-    f 0 z = z^2 `mod` m
-    f 1 z = f 0 z * b `mod` m
-
-prop_powerModulo :: BigInt100000 -> BigInt100000 -> BigInt100000 -> Property
+prop_powerModulo :: BigInt5 -> BigInt5 -> BigInt5 -> Property
 prop_powerModulo b e m = e >= 0 && m >= 2 ==> correct
   where
-    r = powerModulo b e m
-    correct = r == (b^e `mod` m) && r >= 0 && r < m
+    pm    = powerModulo b e m
+    naive = (b^e `mod` m)
+    correct = pm == naive && pm >= 0 && pm < m
