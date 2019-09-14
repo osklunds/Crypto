d7b5aa33df5b7377637faff6a5bfcc0948eb004e
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-09-14 11:52:15 +0200

    Polish and remove monads from Math.Prime.

--- src/Math/Prime.hs
+++ src/Math/Prime.hs
@@ -5,32 +5,43 @@ module Math.Prime
 where
 
 import System.Random
-import Control.Monad
-import Control.Monad.Random.Class
-import Control.Monad.Random.Lazy
 import Test.QuickCheck
 import Prelude hiding (take)
+import Data.Bits
 
 import Math.Divisibility
 import Math.PowerModulo
 import Math.BigInt
 import Tools
 
+-- The final prime function. To provide a simpler interface,
+-- the randomness is hard-coded.
+prime :: (Bits a, Integral a, Random a) => a -> Bool
+prime n
+  | n <  10000 = primeDet n
+  | n >= 10000 = fst $ millerRabin n 64 $ mkStdGen 123
+  -- A cutoff is used. Below, faster with deterministic. Above,
+  -- faster with Miller-Rabin.
+
 primeDet :: Integral a => a -> Bool
 primeDet n
   | n < 2  = False
-  | n >= 2 = null . filter (`divides` n) $ [2..n-1]
+  | n >= 2 = null divisors
+  where
+    divisors = filter (`divides` n) [2..n-1]
 
-
--- Finds (d,s) s.t. n-1 = d*2^s, d odd.
-findDS :: Integral a => a -> (a, a)
-findDS n = head [(d, s) | s <- [0..], 
-                        let d = (n-1) `div` (2^s), 
-                        n-1 == d*2^s,
-                        odd d]
+-- millerRabin n k g does k MR rounds testing n for primality.
+millerRabin :: (RandomGen g, Random a, Integral a, Bits a) =>
+  a -> a -> g -> (Bool, g)
+millerRabin _ 0 g = (True, g)
+millerRabin n k g = (this && rest, g'')
+  where
+    (a, g')     = randomR (1, n-1) g
+    this        = millerRabinOnce n a
+    (rest, g'') = millerRabin n (k-1) g'
 
 -- millerRabinOnce n a does one MR round on n using a.
-millerRabinOnce :: Integral a => a -> a -> Bool
+millerRabinOnce :: (Bits a, Integral a) => a -> a -> Bool
 millerRabinOnce n a
   | even n           = False
   | otherwise        = not (test1 && test2)
@@ -40,31 +51,22 @@ millerRabinOnce n a
     test2 = and $ map (\t -> powerModulo a ((2^t)*d) n /= n-1) 
                       [0..s-1]
 
+-- Finds (d,s) s.t. n-1 = d*2^s, d odd.
+findDS :: Integral a => a -> (a, a)
+findDS n = head [(d, s) | s <- [0..], 
+                        let d = (n-1) `div` (2^s), 
+                        n-1 == d*2^s,
+                        odd d]
+
+prop_millerRabinOnce :: Bool
+prop_millerRabinOnce = and $ map equal [4..5000]
+  where
+    equal :: Integer -> Bool
+    equal n = millerRabinAll n == primeDet n
+
 -- Does MR test on n with all test values a, i.e. a in [1..n-1].
-millerRabinAll :: Integral a => a -> Bool
-millerRabinAll n
-  | n <= 3    = error "Too small for Miller-Rabin"
-  | otherwise = and $ map (millerRabinOnce n) [1..n-1]
+millerRabinAll :: (Bits a, Integral a) => a -> Bool
+millerRabinAll n = and $ map (millerRabinOnce n) [1..n-1]
 
-prop_millerRabinAll :: Int -> Bool
-prop_millerRabinAll k = and $ map (\n -> millerRabinAll n == 
-                                   primeDet n) [4..k]
-
--- millerRabin n k does k MR rounds testing n for primality.
-millerRabin :: (Random a, Integral a, MonadRandom m) =>
-  a -> a -> m Bool
-millerRabin n k = do
-  as <- getRandomRs (1, n-1)
-  return . and . take k $ map (millerRabinOnce n) as
-
--- The final prime function. To provide a simpler interface,
--- the randomness is hard-coded.
-prime :: (Integral a, Random a) => a -> Bool
-prime n
-  | n <  10000 = primeDet n
-  | n >= 10000 = fst $ runRand (millerRabin n 64) $ mkStdGen 123
-  -- A cutoff is used. Below, faster with deterministic. Above,
-  -- faster with Miller-Rabin.
-
-prop_prime :: BigInt100000 -> Bool
+prop_prime :: BigInt5 -> Bool
 prop_prime n = prime n == primeDet n
