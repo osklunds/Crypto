2f4e26db8b50e059b51a0cf881b2c9f2ee0c38bb
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-09-29 10:28:53 +0200

    Polish SHA256 RepConv

--- src/SHA256/RepConv.hs
+++ src/SHA256/RepConv.hs
@@ -2,8 +2,8 @@
 module SHA256.RepConv
 ( asciiToW8list
 , w8x4ToW32
-, w32x8ToHex
 , w64ToW8x8
+, w32x8ToHex
 )
 where
 
@@ -12,27 +12,30 @@ import Data.Word
 import Data.Bits
 import Prelude hiding (take, length)
 
-import SHA256.Types
 import Tools
 
-asciiToW8list :: String -> ByteString
+-- In all functions, MSB is considered.
+
+asciiToW8list :: String -> [Word8]
 asciiToW8list = map (fromIntegral . ord)
+
 
 w8x4ToW32 :: [Word8] -> Word32
 w8x4ToW32 = w8x4ToW32' 24
 
-w8x4ToW32' :: Int -> ByteString -> Word32
+w8x4ToW32' :: Int -> [Word8] -> Word32
 w8x4ToW32' _ []     = 0
-w8x4ToW32' n (b:bs) = (fromIntegral b) `shiftL` n +
-                      w8x4ToW32' (n-8) bs
+w8x4ToW32' n (b:bs) = (fromIntegral b) `shiftL` n + w8x4ToW32' (n-8) bs
+
+
+w64ToW8x8 :: Word64 -> [Word8]
+w64ToW8x8 w64 = [fromIntegral (w64 `shiftR` i) | i <- [56,48..0]]
+
+
+w32x8ToHex :: [Word32] -> String
+w32x8ToHex = concat . map (prependZeros . toHex)
 
 prependZeros :: String -> String
 prependZeros str = replicate missing '0' ++ str
   where
     missing = 8 - length str
-
-w32x8ToHex :: Digest -> String
-w32x8ToHex = concat . map (prependZeros . toHex)
-
-w64ToW8x8 :: Word64 -> ByteString
-w64ToW8x8 w64 = [fromIntegral (w64 `shiftR` i) | i <- [56,48..0]]
