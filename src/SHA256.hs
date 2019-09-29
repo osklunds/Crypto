a3271cc1d9c5df5898e7a402434f7aadab8a3d71
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-09-29 10:45:08 +0200

    Polish the main SHA256 function.

--- src/SHA256.hs
+++ src/SHA256.hs
@@ -8,21 +8,23 @@ import Data.Word
 import Data.Bits
 import Tools
 
-import SHA256.Types
 import SHA256.Padding
 import SHA256.Constants
 import SHA256.Compression
 import SHA256.MessageSchedule
 import SHA256.RepConv
 
-processChunks :: Digest -> [Chunk] -> Digest
-processChunks d []     = d
-processChunks d (c:cs) = processChunks (compress ws d) cs
+sha256 :: String -> String
+sha256 = w32x8ToHex . processChunks initialHash . chunk . pad . asciiToW8list
+
+-- Splits the padded bit string into chunks, each 512 bits.
+chunk :: [Word8] -> [[Word32]]
+chunk = group 16 . map w8x4ToW32 . group 4
+
+processChunks :: [Word32] -> [[Word32]] -> [Word32]
+processChunks = foldl processChunk
+
+processChunk :: [Word32] -> [Word32] -> [Word32]
+processChunk d c = compress ws d
   where
     ws = messageSchedule c
-
-padAndChunk :: ByteString -> [Chunk]
-padAndChunk = group 16 . map w8x4ToW32 . group 4 . pad
-
-sha256 :: String -> String
-sha256 = w32x8ToHex . processChunks initialHash . padAndChunk . asciiToW8list
