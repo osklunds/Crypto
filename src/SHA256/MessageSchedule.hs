59a04803891c9023aed96df1a2017b66809b59aa
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-09-29 10:18:19 +0200

    Polish SHA256 MessageSchedule.

--- src/SHA256/MessageSchedule.hs
+++ src/SHA256/MessageSchedule.hs
@@ -7,23 +7,23 @@ where
 import Data.Word
 import Data.Bits
 
-import SHA256.Types
+-- chunk is of length 16. So 512 bits in total.
+messageSchedule :: [Word32] -> [Word32]
+messageSchedule chunk = let ws = chunk ++ map w [16..63]
+                            w i = w16 + s0 w15 + w7 + s1 w2
+                              where
+                                w16 = ws !! (i-16)
+                                w15 = ws !! (i-15)
+                                w7  = ws !! (i-7)
+                                w2  = ws !! (i-2)
+                        in ws
 
 s0 :: Word32 -> Word32
 s0 w15 = (w15 `rotateR` 7 ) `xor`
          (w15 `rotateR` 18) `xor`
          (w15 `shiftR`  3 )
+
 s1 :: Word32 -> Word32
 s1 w2  = (w2  `rotateR` 17) `xor`
          (w2  `rotateR` 19) `xor`
          (w2  `shiftR`  10)
-
-messageSchedule :: Chunk -> RoundWords
-messageSchedule chunk = let list = chunk ++ map ms [16..63]
-                            ms i = w16 + s0 w15 + w7 + s1 w2
-                              where
-                                w16 = list !! (i-16)
-                                w15 = list !! (i-15)
-                                w7  = list !! (i-7)
-                                w2  = list !! (i-2)
-                        in list
